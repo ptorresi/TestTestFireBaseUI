ec036727de5deec36a85205dd4da44074b614e6b
Pablo Torresi <pmtorresi@gmail.com>
2020-06-07 19:42:55 -0300

    Initial commit

---
+++ app/src/main/java/com/sirpla/testfirebaseui/InsertRecommendations.java
@@ -0,0 +1,14 @@
+package com.sirpla.testfirebaseui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertRecommendations extends AppCompatActivity {
+     
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate( savedInstanceState );
+          setContentView( R.layout.activity_insert_recommendations );
+     }
+}
